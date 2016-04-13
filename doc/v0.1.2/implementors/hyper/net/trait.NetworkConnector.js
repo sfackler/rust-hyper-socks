994a5008ab5ea72e911136e4945cdaa365317beb
Steven Fackler <sfackler@palantir.com>
2016-04-12 17:22:46 -0700

    Release v0.1.2

---
+++ doc/v0.1.2/implementors/hyper/net/trait.NetworkConnector.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['hyper_socks'] = ["impl <a class='trait' href='https://hyperium.github.io/hyper/hyper/net/trait.NetworkConnector.html' title='hyper::net::NetworkConnector'>NetworkConnector</a> for <a class='struct' href='hyper_socks/struct.Socks4HttpConnector.html' title='hyper_socks::Socks4HttpConnector'>Socks4HttpConnector</a>","impl&lt;S: <a class='trait' href='https://hyperium.github.io/hyper/hyper/net/trait.Ssl.html' title='hyper::net::Ssl'>Ssl</a>&gt; <a class='trait' href='https://hyperium.github.io/hyper/hyper/net/trait.NetworkConnector.html' title='hyper::net::NetworkConnector'>NetworkConnector</a> for <a class='struct' href='hyper_socks/struct.Socks4HttpsConnector.html' title='hyper_socks::Socks4HttpsConnector'>Socks4HttpsConnector</a>&lt;S&gt;","impl <a class='trait' href='https://hyperium.github.io/hyper/hyper/net/trait.NetworkConnector.html' title='hyper::net::NetworkConnector'>NetworkConnector</a> for <a class='struct' href='hyper_socks/struct.Socks5HttpConnector.html' title='hyper_socks::Socks5HttpConnector'>Socks5HttpConnector</a>","impl&lt;S: <a class='trait' href='https://hyperium.github.io/hyper/hyper/net/trait.Ssl.html' title='hyper::net::Ssl'>Ssl</a>&gt; <a class='trait' href='https://hyperium.github.io/hyper/hyper/net/trait.NetworkConnector.html' title='hyper::net::NetworkConnector'>NetworkConnector</a> for <a class='struct' href='hyper_socks/struct.Socks5HttpsConnector.html' title='hyper_socks::Socks5HttpsConnector'>Socks5HttpsConnector</a>&lt;S&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
